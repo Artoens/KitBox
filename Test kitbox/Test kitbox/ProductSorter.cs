43ad642d459d9dc2f7c6858e19bc85b0053e21e2
Jordan JACOBI <17183@ecam.be>
2018-03-14 11:39:58 +0100

    Add a Sorter class

---
+++ Test kitbox/Test kitbox/ProductSorter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_kitbox
+{
+    public static class ProductSorter
+    {
+        public static List<Product> AvoidDoubles(List<Product> productList)
+        {
+            List<Product> newList = new List<Product>();
+
+            foreach(Product p in productList)
+            {
+
+            }
+
+            return productList;
+        }
+    }
+}
