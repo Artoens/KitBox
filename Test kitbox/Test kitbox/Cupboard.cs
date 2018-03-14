8254769717b2d5ab28f9e4e74e3ce182d3cf8c17
Martin DEGELDT <15076@ecam.be>
2018-03-14 15:08:51 +0100

    fix minor bugs

--- Test kitbox/Test kitbox/Cupboard.cs
+++ Test kitbox/Test kitbox/Cupboard.cs
@@ -43,7 +43,8 @@ namespace Test_kitbox
                 compartments[index].Height,
                 compartments[index].MainColor,
                 compartments[index].DoorPresence,
-                compartments[index].DoorColor);
+                compartments[index].DoorColor,
+                this);
         }
 
         public int NumberCompartments
