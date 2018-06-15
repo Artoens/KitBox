ddd70d24d3216523fe09c3e554e478f42c977d5e
Sam Bertrand <15027@ecam.be>
2018-06-15 17:21:53 +0200

    add of the dbcontroller.cs for db in maga

---
+++ KitBoxMag/KitBoxMag/KitBoxMag/DBController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitBoxMag
+{
+    class DBController
+    {
+        List<Piece> GetAllPiecesOrdered()
+        {
+
+        }
+
+        List<Piece> GetAllStock()
+        {
+
+        }
+
+        List<ClientOrder> GetAllClientsOrder()
+        {
+
+        }
+
+        void OrderPiece(string Piece_Code)
+        {
+
+        }
+
+        void DeletePieceOrdered(string Piece_Code)
+        {
+
+        }
+
+        void DeleteClientOrder(char ID_Order)
+        {
+
+        }
+
+        List<Piece> GetAllPiecesToOrder()
+        {
+
+        }
+    }
